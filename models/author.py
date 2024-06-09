6a49db4e07ca6767e7a256dfa849e4f5749df5e5
Stephen Ochieng <materiamystic@gmail.com>
2024-06-10 02:37:47 +0300

    Adds articles and magazines methods

--- models/author.py
+++ models/author.py
@@ -1,5 +1,10 @@
 from database.connection import get_db_connection
+
+conn = get_db_connection()
+cursor = conn.cursor()
 class Author:
+    
+    all = {}
     def __init__(self, name, id=None):
         self.id = id
         self.name = name
@@ -33,11 +38,61 @@ class Author:
             raise TypeError("Name must be of type str and longer than 0 characters")
        
     def save(self):
-        conn = get_db_connection()
-        cursor = conn.cursor()
+        
         cursor.execute('INSERT INTO authors (name) VALUES (?)', (self.name,))
         conn.commit()
         conn.close()
         
         self.id = cursor.lastrowid
         
+        
+    @classmethod
+    def instance_from_db(cls, row):
+        """Return an author object having the attribute values from the table row."""
+
+        # Check the dictionary for  existing instance using the row's primary key
+        author = cls.all.get(row[0])
+        if author:
+            # ensure attributes match row values in case local instance was modified
+            author.name = row[1]
+            
+        else:
+            # not in dictionary, create new instance and add to dictionary
+            author = cls(row[1])
+            author.id = row[0]
+            cls.all[author.id] = author
+        return author
+        
+        
+    def articles(self):
+        """Returns all articles associated with an author"""
+        from models.article import Article
+        
+        sql = """
+        SELECT * FROM articles
+        WHERE author_id = ?
+        """
+        
+        rows = cursor.execute(sql, (self.id,)).fetchall()
+        
+        return [Article.instance_from_db(row) for row in rows]
+    
+    def magazines(self):
+        """Returns all magazines associated with an author"""
+        
+        from models.magazine import Magazine
+        
+        sql = """
+        SELECT magazines.id, magazines.name, magazines.category FROM magazines
+        INNER JOIN articles
+        ON magazines.id = articles.magazine_id
+        INNER JOIN authors
+        ON articles.author_id = authors.id
+        WHERE authors.id = ?
+        """
+        
+        rows = cursor.execute(sql, (self.id,)).fetchall()
+        
+        return [Magazine.instance_from_db(row) for row in rows]
+    
+    
